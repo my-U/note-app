8507571a98b5f93e9c38b74a7a262e137584c00f
minyoung <myouth_u@naver.com>
2026-04-11 03:54:04 +0900

    feat: AuthModule 등록

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { MemoService } from './memo/service/memo.service';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {ConfigModule} from "@nestjs/config";
 import {MemoModule} from "./memo/memo.module";
+import {AuthModule} from "./auth/auth.module";
 
 @Module({
   imports: [
@@ -18,7 +19,8 @@ import {MemoModule} from "./memo/memo.module";
         entities: [__dirname + '/**/*.entity{.ts,.js}'], // entity 파일을 찾아서 TypeORM에 인식시키는 설정 (테이블 생성/수정 대상 지정)
         synchronize: true  // entity 파일을 기반으로 DB 테이블을 자동으로 생성/수정해주는 설정
       }),
-      MemoModule // memo.module.ts 등록
+      MemoModule, // memo.module.ts 등록
+      AuthModule // auth.module.ts 등록
   ],
 })
 export class AppModule {}
